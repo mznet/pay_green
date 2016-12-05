a151a18cd28f1cff6f3073209883316e96088fcd
mjet <mjet@i-um.net>
2016-12-05 23:44:48 +0900

    remove spree namespace in blowfish.rb

--- lib/blowfish.rb
+++ lib/blowfish.rb
@@ -1,19 +1,16 @@
 require 'mcrypt'
 
-module Spree
-  class Blowfish
-    def initialize(key)
-      @key = key
-      @crypto = Mcrypt.new(:blowfish, :ecb, key)
-    end
+class Blowfish
+  def initialize(key)
+    @key = key
+    @crypto = Mcrypt.new(:blowfish, :ecb, key)
+  end
 
-    def encrypt(data, encoding_type = 'utf-8')
-      @crypto.encrypt(data).force_encoding(encoding_type)
-    end
+  def encrypt(data, encoding_type = 'utf-8')
+    @crypto.encrypt(data).force_encoding(encoding_type)
+  end
 
-    def decrypt(data)
-      @crypto.decrypt(data)
-    end
-
+  def decrypt(data)
+    @crypto.decrypt(data)
   end
 end
